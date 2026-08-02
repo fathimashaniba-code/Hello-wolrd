701af0183a253b3239e582d6707342d2a075a09f
fathimashaniba-code <fathimashaniba.m12@gmail.com>
2026-08-02 13:22:12 +0530

    Grade calculator.py

---
+++ Grade calculator.py
@@ -0,0 +1,15 @@
+n = int(input("Enter your score in 100:"))
+if (n>=90):
+print ("A+")
+elif (n>80):
+print ("A")
+elif (n>70):
+print("B+")
+elif(n>60):
+print("B")
+elif(n>50):
+print("C+")
+elif(n>40):
+print("PASS")
+else:
+print("FAIL")
