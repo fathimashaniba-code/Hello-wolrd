5375a3d63c8b179c4becd6e42c7fee477dba2d93
fathimashaniba-code <fathimashaniba.m12@gmail.com>
2026-08-02 15:02:55 +0530

    Mini calculator.py

---
+++ Mini calculator.py
@@ -0,0 +1,13 @@
+a = int(input("enter the first value"))
+b = int(input("enter the second value"))
+c = a+b
+d = a-b
+e = a*b
+f = a/b
+print("sum is",c)
+print("difference is",d)
+print("product is",e)
+if (b==0):
+ print("quotient is",f)
+else:
+ print("quotient is", Zero)
