380962556ab492e6741035cdb75a534bba8104f0
fathimashaniba-code <fathimashaniba.m12@gmail.com>
2026-08-02 15:15:02 +0530

    Mini calculator.py

--- Mini calculator.py
+++ Mini calculator.py
@@ -3,11 +3,9 @@ b = int(input("enter the second value"))
 c = a+b
 d = a-b
 e = a*b
-f = a/b
 print("sum is",c)
 print("difference is",d)
-print("product is",e)
 if (b==0):
- print("quotient is",f)
+  print("quotient is zero")
 else:
- print("quotient is", Zero)
+  print("quotient is",a/b)
